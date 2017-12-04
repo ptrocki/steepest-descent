81e3eae006964456ea42009b932cc9ec167e4201
Piotr Trocki <ptrocki@gmail.com>
2017-12-04 10:01:34 +0100

    change alpha

--- script.m
+++ script.m
@@ -1,8 +1,9 @@
 
-results =zeros(1,8);
+results =zeros(1,7);
 
 f = sym('5*(a-2)^2+7*(b-2)^2');
 grad = gradient(f);
+
 syms a
 syms b
 ypsylon = 10^(-1);
@@ -10,17 +11,15 @@ ypsylon = 10^(-1);
 k = 0; 
 x = [4 4]';
     
-
-
 while 1>0
     x_prior = x;
     k = k+1;
-    d = -1 * subs(grad,[a b], x'); % find new direction
-    alpha = ( d(1) * (14 - 20*x(1) -10 * x(2)) +d(2)*( 6 - 10*x(1)-16*x(2)) ) / ( 20*d(1)^2 + 20*d(1)*d(2) + 16*d(2)^2);
+    d = -1 * subs(grad,[a b], x'); 
+    alpha = 0.1;
     
     x = x + alpha .* (d);
   
-    results(k,:) = [k, x_prior(1), x_prior(2), d(1), d(2), norm(d), alpha, subs(f,[a b], x_prior') ];
+    results(k,:) = [k, x_prior(1), x_prior(2), d(1), d(2), norm(d), subs(f,[a b], x_prior') ];
 
     if norm(x - x_prior,2) < ypsylon
         break;

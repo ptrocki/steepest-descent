de207199660025bd4b5bff7482f32202caaa20f5
Piotr Trocki <ptrocki@gmail.com>
2017-12-14 18:31:34 +0100

    Change algo

--- fun.m
+++
@@ -1,4 +0,0 @@
-function y = fun(x1,x2,a,b,c,d,e,f)
-    y = a*(x1-b)^c+d*(x2-e)^f;
-end
-
--- script.m
+++ script.m
@@ -10,16 +10,16 @@ syms tau
 ypsylon = 10^(-2);
 
 k = 0; 
-x = [1 1]';
+x = [3 3]';
     
 while 1>0
     x_temp = x;
     k = k+1;
     d = -1 * subs(grad,[a b], x'); 
-    alpha = 0.1;
     x_tau = x + tau.*d;
     func_to_opt2 = subs(f,[a b], x_tau');
-    x = armijo(matlabFunction(func_to_opt2),x(1)-30,x(2)+30);
+    min_of_tau_func = simulannealbnd(matlabFunction(func_to_opt2),double(x(1)));
+    x = x + d.*min_of_tau_func;
     results(k,:) = [k, x_temp(1), x_temp(2), d(1), d(2), subs(f,[a b], x_temp') ];
     if norm(x - x_temp) < ypsylon
         disp(results(k,:))
